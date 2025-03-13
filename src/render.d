e935cb9b12ecd50e9f31e726b158fc14f0afd387
tspike <mail@gamedevgp.net>
2025-03-13 15:24:52 -0400

    Revising license headers.

--- src/render.d
+++ src/render.d
@@ -1,7 +1,7 @@
 /*
-Authors:   tspike (github.com/tspike2k)
-Copyright: Copyright (c) 2025
-License:   Boost Software License 1.0 (https://www.boost.org/LICENSE_1_0.txt)
+Copyright (c) 2025 tspike2k@github.com
+Distributed under the Boost Software License, Version 1.0.
+See accompanying file LICENSE_BOOST.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 */
 
 /+
